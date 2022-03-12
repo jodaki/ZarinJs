62fc51c795223e41b4c5098199b441027093ac7e
mohammad joudaki <75210468+rudoxman@users.noreply.github.com>
2022-03-13 02:00:48 +0330

    update

--- src/Page/Users.jsx
+++ src/Page/Users.jsx
@@ -1,47 +1,57 @@
 import React, { Component, setState} from 'react'
 import axios from 'axios'
+
+
+
 export default class Users extends Component {
 
   state = {
-   users: []
+   users: [], 
+   isloader: true
   }
-  // async componentDidMount(){
-  //   const respanse = await axios.get('https://reqres.in/api/users')
-  //   console.log(respanse);
-  //   this.setState({users: respanse.data.data})
+  async componentDidMount(){
+    const respanse = await axios.get('https://reqres.in/api/users')
+    setTimeout(() => {
+      this.setState({users: respanse.data.data, isloader: false})
+    }, 2000);
 
+  }
+  // componentDidMount(){
+  //   axios.get('https://reqres.in/api/users')
+  //   .then(res => {
+  //     const users = res.data.data
+  //     this.setState({users})
+  //   })
   // }
-  componentDidMount(){
-    axios.get('https://reqres.in/api/users')
-    .then(res => {
-      const users = res.data.data
-      this.setState({users})
-    })
-  }
   render() {
+
+    const isloader = this.state.isloader
     return (
-      <>
-        <button onClick={this.handleCreate} className='btn btn-primary btn-sm m-4'>Update</button>
-        <div className="row">
-        {
-          this.state.users.map((user)=>{
-            return(
-              <div className='col-4'> 
-                <img  className='rounded-circle' src={user.avatar} alt="avatar" />
-                <p>Name: {user.first_name}{user.last_name}</p>
-                <span>id: {user.id}</span>
-                
-              </div>
-            )
-          })
-
-
-        }            
-        </div>
-      </>
+      <div>
+        { 
+          isloader ? (<h1 className='text-center display-6 border m-6'>loding..</h1>) :(
+            <div className="row">
+              {
+                this.state.users.map((user)=>{
+                  return(
+                      <div className='col-4'> 
+                        <img  className='rounded-circle' src={user.avatar} alt="avatar" />
+                        <p>Name: {user.first_name}{user.last_name}</p>
+                        <span>id: {user.id}</span>
+                      </div>
+                  )
+                })
+              }
+            </div>
+          )
+          
+        }         
+        
+      </div>
     )
   }
  
 }
 
-
+{/* <button onClick={this.handleCreate} className='btn btn-primary btn-sm m-4'>Update</button>
+        <div className="row"></div> */}
